667b5c3434b97cdb625ea662ee11f5588df8ae98
Alessandro <la-tua-email-github@example.com>
2026-04-25 14:49:35 +0200

    fix keycloak config syntax

--- src/app/keycloak.config.ts
+++ src/app/keycloak.config.ts
@@ -1,38 +1,32 @@
 import {
-    provideKeycloak,
-    createInterceptorCondition,
-    IncludeBearerTokenCondition,
-    INCLUDE_BEARER_TOKEN_INTERCEPTOR_CONFIG,
-    withAutoRefreshToken,
-    AutoRefreshTokenService,
-    UserActivityService
+  provideKeycloak,
+  withAutoRefreshToken,
+  AutoRefreshTokenService,
+  UserActivityService
 } from 'keycloak-angular';
 import { KeycloakOnLoad } from 'keycloak-js';
 import { environment } from 'src/environments/environment';
 
 export const provideKeycloakAngular = () =>
-    provideKeycloak({
-        config: {
-            url: environment.keycloak.config.url,
-            realm: environment.keycloak.config.realm,
-            clientId: environment.keycloak.config.clientId,
-            
-        },
-        initOptions: {
-            onLoad: 'login-required' as KeycloakOnLoad,
-            silentCheckSsoRedirectUri: environment.keycloak.initOptions.silentCheckSsoRedirectUri,
-            redirectUri: environment.keycloak.initOptions.redirectUri
-       },
-        features: [
-            withAutoRefreshToken({
-                onInactivityTimeout: 'logout',
-                sessionTimeout: 1000
-            })
-        ],
-        providers: [
-  AutoRefreshTokenService,
-  UserActivityService
-],
-            }
-        ]
-    });
+  provideKeycloak({
+    config: {
+      url: environment.keycloak.config.url,
+      realm: environment.keycloak.config.realm,
+      clientId: environment.keycloak.config.clientId,
+    },
+    initOptions: {
+      onLoad: 'login-required' as KeycloakOnLoad,
+      silentCheckSsoRedirectUri: environment.keycloak.initOptions.silentCheckSsoRedirectUri,
+      redirectUri: environment.keycloak.initOptions.redirectUri
+    },
+    features: [
+      withAutoRefreshToken({
+        onInactivityTimeout: 'logout',
+        sessionTimeout: 1000
+      })
+    ],
+    providers: [
+      AutoRefreshTokenService,
+      UserActivityService
+    ]
+  });
