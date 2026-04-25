1656227260d22fc7224fb85ad1f6a81e1f65b3f8
Alessandro <la-tua-email-github@example.com>
2026-04-25 15:38:53 +0200

    add keycloak debug log

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,21 +1,42 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideAppInitializer, inject, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
-import MyPreset from './mypreset';
-import { includeBearerTokenInterceptor, provideKeycloak } from 'keycloak-angular';
-import { provideKeycloakAngular } from '@/keycloak.config';
-export const appConfig: ApplicationConfig = {
-    providers: [
-        provideRouter(routes),
-        provideKeycloakAngular(),
-        provideHttpClient(withInterceptors([includeBearerTokenInterceptor])),
-        provideAnimationsAsync(),
-        providePrimeNG({ theme: { preset: MyPreset, options: { darkModeSelector: '.app-dark' } } }),
-        provideBrowserGlobalErrorListeners(),
-        provideZonelessChangeDetection(),
-    ]
-};
+import Keycloak from 'keycloak-js';
 
+import { routes } from './app.routes';
+import MyPreset from './mypreset';
+import { includeBearerTokenInterceptor } from 'keycloak-angular';
+import { provideKeycloakAngular } from './app/keycloak.config';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+
+    provideKeycloakAngular(),
+
+    provideHttpClient(withInterceptors([includeBearerTokenInterceptor])),
+
+    provideAnimationsAsync(),
+
+    providePrimeNG({
+      theme: {
+        preset: MyPreset,
+        options: {
+          darkModeSelector: '.app-dark'
+        }
+      }
+    }),
+
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+
+    provideAppInitializer(() => {
+      const kc = inject(Keycloak);
+      console.log('[KC] presente:', !!kc);
+      console.log('[KC] authenticated:', kc.authenticated);
+      console.log('[KC] token:', !!kc.token);
+    })
+  ]
+};
